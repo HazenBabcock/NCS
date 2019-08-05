0eafc163a8f474ce77a4ecb8f59a549443121947
Hazen Babcock <hbabcock@mac.com>
2019-08-05 13:56:54 -0400

    Add Python version of OpenCL ncsReduceNoise().

--- opencl/pyOpenCLNCS/py_ref.py
+++ opencl/pyOpenCLNCS/py_ref.py
@@ -16,10 +16,11 @@ import numpy
 FITMIN = numpy.float32(1.0e-6)
 PSIZE = 64
 
+## L-BFGS solver parameters.
 C_1  = numpy.float32(1.0e-4)
-EPSILON = numpy.float32(1.0e-5)
+EPSILON = numpy.float32(1.0e-4)
 MAXITERS = 200
-
+M = 8
 MIN_STEP = numpy.float32(1.0e-6)
 STEPM = numpy.float32(0.5)
 
@@ -29,6 +30,7 @@ UNSTARTED = -1
 REACHED_MAXITERS = -2
 INCREASING_GRADIENT = -3
 MINIMUM_STEP = -4
+REACHED_MAXPRECISION = -5
 
 
 ## FFT functions.
@@ -109,6 +111,23 @@ def converged(x, g):
     else:
         return True
 
+
+## Python specific helper functions.
+
+def createUFFTGrad():
+    u_fft_grad_r = []
+    u_fft_grad_c = []
+
+    for i in range(4*PSIZE):
+        u_fft_grad_r.append(None)
+        u_fft_grad_c.append(None)
+
+    initUFFTGrad(u_fft_grad_r, u_fft_grad_c)
+    return [u_fft_grad_r, u_fft_grad_c]
+
+
+## "Kernels"
+
 def initUFFTGrad(u_fft_grad_r, u_fft_grad_c):
     x_r = numpy.zeros(4*PSIZE, dtype = numpy.float32)
     x_c = numpy.zeros(4*PSIZE, dtype = numpy.float32)
@@ -121,15 +140,181 @@ def initUFFTGrad(u_fft_grad_r, u_fft_grad_c):
         u_fft_grad_r[i] = numpy.copy(y_r)
         u_fft_grad_c[i] = numpy.copy(y_c)
         x_r[i] = 0.0
+        
+def ncsReduceNoise(u_fft_grad_r,
+                   u_fft_grad_c,
+                   data_in,
+                   g_gamma,
+                   otf_mask,
+                   data_out,
+                   iterations,
+                   status,
+                   alpha):
 
-# Python specific helper functions.
-def createUFFTGrad():
-    u_fft_grad_r = []
-    u_fft_grad_c = []
+    g_id = 0
+    status[g_id] = UNSTARTED
+    offset = g_id*PSIZE
 
-    for i in range(4*PSIZE):
-        u_fft_grad_r.append(None)
-        u_fft_grad_c.append(None)
+    a = numpy.zeros(M, dtype = numpy.float32)
+    ys = numpy.zeros(M, dtype = numpy.float32)
+    
+    data = numpy.zeros(4*PSIZE, dtype = numpy.float32)
+    gamma = numpy.zeros(4*PSIZE, dtype = numpy.float32)
+    g_p = numpy.zeros(4*PSIZE, dtype = numpy.float32)
+    gradient = numpy.zeros(4*PSIZE, dtype = numpy.float32)
+    otf_mask_sqr  = numpy.zeros(4*PSIZE, dtype = numpy.float32)
+    srch_dir = numpy.zeros(4*PSIZE, dtype = numpy.float32)
+    u_r = numpy.zeros(4*PSIZE, dtype = numpy.float32)
+    u_c = numpy.zeros(4*PSIZE, dtype = numpy.float32)
+    u_fft_r = numpy.zeros(4*PSIZE, dtype = numpy.float32)
+    u_fft_c = numpy.zeros(4*PSIZE, dtype = numpy.float32)
+    u_p = numpy.zeros(4*PSIZE, dtype = numpy.float32)
+    work1  = numpy.zeros(4*PSIZE, dtype = numpy.float32)
 
-    initUFFTGrad(u_fft_grad_r, u_fft_grad_c)
-    return [u_fft_grad_r, u_fft_grad_c]
+    s = []
+    y = []
+    for i in range(M):
+        s.append(numpy.zeros(4*PSIZE, dtype = numpy.float32))
+        y.append(numpy.zeros(4*PSIZE, dtype = numpy.float32))
+
+    # Initialization.
+    for i in range(PSIZE*4):
+        data[i] = data_in.flatten()[i + offset]
+        gamma[i] = g_gamma.flatten()[i]
+        otf_mask_sqr[i] = otf_mask.flatten()[i] * otf_mask.flatten()[i]
+        u_r[i] = data_in.flatten()[i + offset]
+        u_c[i] = 0.0
+    
+    # Calculate initial state.
+    fft_16x16(u_r, u_c, u_fft_r, u_fft_c)
+    
+    # Cost.
+    cost = calcLogLikelihood(u_r, data, gamma)
+    cost += alpha * calcNoiseContribution(u_fft_r, u_fft_c, otf_mask_sqr)
+    
+    # Gradient.
+    calcLLGradient(u_r, data, gamma, gradient);
+    calcNCGradient(u_fft_grad_r, u_fft_grad_c, u_fft_r, u_fft_c, otf_mask_sqr, work1);
+    vecfmaInplace(gradient, work1, alpha);
+    
+    # Check if we've already converged.
+    if (converged(u_r, gradient)):
+        for i in range(PSIZE*4):
+            data_out[i + offset] = u_r[i]
+        iterations[g_id] = 1
+        status[g_id] = SUCCESS
+        return
+    
+    # Initial search direction.
+    step = 1.0/vecnorm(gradient)
+    vecncopy(srch_dir, gradient)
+
+    # Start search.
+    for k in range(MAXITERS):
+    
+        # 
+        # Line search. 
+        #
+        # This checks the Armijo rule/condition.
+        # https://en.wikipedia.org/wiki/Wolfe_conditions
+        #
+        t1 = C_1 * vecdot(srch_dir, gradient)
+         
+        if (t1 > 0.0):
+            # Increasing gradient. Minimization failed.
+            for i in range(PSIZE*4):
+                data_out[i + offset] = u_r[i]
+            iterations[g_id] = k+1
+            status[g_id] = INCREASING_GRADIENT
+            return
+        
+        # Store current cost, u and gradient.
+        cost_p = cost
+        veccopy(u_p, u_r)
+        veccopy(g_p, gradient)
+
+	# Search for a good step size.
+        searching = 1
+        while(searching):
+        
+            # Move in search direction.
+            vecfma(u_r, srch_dir, u_p, step)
+            
+            # Calculate new cost.
+            fft_16x16(u_r, u_c, u_fft_r, u_fft_c)
+            cost = calcLogLikelihood(u_r, data, gamma)
+            cost += alpha * calcNoiseContribution(u_fft_r, u_fft_c, otf_mask_sqr)
+            
+            # Armijo condition.
+            if (cost <= (cost_p + t1*step)):
+                searching = 0;
+            else:
+                step = STEPM*step
+                if (step < MIN_STEP):
+                    #
+                    # Reached minimum step size. Minimization failed. 
+                    # Return the last good u values.
+                    #
+                    for i in range(PSIZE*4):
+                        data_out[i + offset] = u_p[i]
+                    iterations[g_id] = k+1
+                    status[g_id] = MINIMUM_STEP
+                    return
+        
+        # Calculate new gradient.
+        calcLLGradient(u_r, data, gamma, gradient)
+        calcNCGradient(u_fft_grad_r, u_fft_grad_c, u_fft_r, u_fft_c, otf_mask_sqr, work1)
+        vecfmaInplace(gradient, work1, alpha)
+        
+        # Convergence check.
+        if (converged(u_r, gradient)):
+            for i in range(PSIZE*4):
+                data_out[i + offset] = u_r[i]
+            iterations[g_id] = k+1
+            status[g_id] = SUCCESS
+            return
+        
+        #
+	# Machine precision check.
+        #
+	# This is probably not an actual failure, we just ran out of digits. Reaching
+	# this state has a cost so we want to know if this is happening a lot.
+        #
+        if vecisEqual(u_r, u_p):
+            for i in range(PSIZE*4):
+                data_out[i + offset] = u_r[i]
+            iterations[g_id] = k+1
+            status[g_id] = REACHED_MAXPRECISION
+            return
+        
+        # L-BFGS calculation of new search direction.
+        ci = (k-1)%M
+        vecsub(s[ci], u_r, u_p)
+        vecsub(y[ci], gradient, g_p)
+        
+        ys_c0 = vecdot(s[ci], y[ci])
+        ys[ci] = 1.0/ys_c0
+        yy = 1.0/vecdot(y[ci], y[ci])
+        
+        vecncopy(srch_dir, gradient)
+        bound = min(k, M)
+        for j in range(bound):
+            ci = (k - j - 1)%M
+            a[ci] = vecdot(s[ci], srch_dir)*ys[ci]
+            vecfmaInplace(srch_dir, y[ci], -a[ci])
+        
+        vecscaleInplace(srch_dir, ys_c0*yy)
+        
+        for j in range(bound):
+            ci = (k + j - bound)%M
+            beta = vecdot(y[ci], srch_dir)*ys[ci]
+            vecfmaInplace(srch_dir, s[ci], (a[ci] - beta))
+        
+        step = 1.0
+    
+    # Reached maximum iterations. Minimization failed.
+    for i in range(PSIZE*4):
+        data_out[i + offset] = u_r[i]
+    iterations[g_id] = MAXITERS
+    status[g_id] = REACHED_MAXITERS
+    
--- opencl/pyOpenCLNCS/test/test_solver.py
+++ opencl/pyOpenCLNCS/test/test_solver.py
@@ -9,6 +9,7 @@ import pyopencl as cl
 
 # python3 and C NCS reference version.
 import pyCNCS.ncs_c as ncsC
+import pyOpenCLNCS.py_ref as pyRef
 
 import pyOpenCLNCS
 
@@ -38,7 +39,84 @@ except:
 alpha = 0.1
 n_pts = 16
 
-def test_ncs_noise_reduction():
+def test_ncs_noise_reduction_1():
+
+   # Setup
+   numpy.random.seed(1)
+
+   data = numpy.random.uniform(low = 10.0, high = 20.0, size = (n_pts, n_pts)).astype(dtype = numpy.float32)
+   gamma = numpy.random.uniform(low = 2.0, high = 4.0, size = (n_pts, n_pts)).astype(dtype = numpy.float32)
+   otf_mask = numpy.random.uniform(size = (n_pts, n_pts)).astype(numpy.float32)
+   otf_mask_shift = numpy.fft.fftshift(otf_mask)
+
+   # OpenCL Setup.
+   u_fft_grad_r = numpy.zeros((n_pts * n_pts, n_pts, n_pts)).astype(numpy.float32)
+   u_fft_grad_c = numpy.zeros((n_pts * n_pts, n_pts, n_pts)).astype(numpy.float32)
+   
+   u_fft_grad_r_buffer = cl.Buffer(context, 
+                                   cl.mem_flags.READ_WRITE | cl.mem_flags.COPY_HOST_PTR, 
+                                   hostbuf = u_fft_grad_r)
+   u_fft_grad_c_buffer = cl.Buffer(context, 
+                                   cl.mem_flags.READ_WRITE | cl.mem_flags.COPY_HOST_PTR, 
+                                   hostbuf = u_fft_grad_c)
+   
+   u = numpy.zeros((n_pts, n_pts), dtype = numpy.float32)
+   iters = numpy.zeros(1, dtype = numpy.int32)
+   status = numpy.zeros(1, dtype = numpy.int32)
+   
+   data_buffer = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, 
+                           hostbuf = data)
+   gamma_buffer = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, 
+                            hostbuf = gamma)
+   otf_mask_buffer = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, 
+                               hostbuf = otf_mask_shift)
+   u_buffer = cl.Buffer(context, cl.mem_flags.WRITE_ONLY | cl.mem_flags.COPY_HOST_PTR, 
+                        hostbuf = u)
+   iters_buffer = cl.Buffer(context, cl.mem_flags.WRITE_ONLY | cl.mem_flags.COPY_HOST_PTR, 
+                            hostbuf = iters)
+   status_buffer = cl.Buffer(context, cl.mem_flags.WRITE_ONLY | cl.mem_flags.COPY_HOST_PTR, 
+                             hostbuf = status)
+
+   # OpenCL noise reduction.
+   program.initUFFTGrad(queue, (1,), (1,), u_fft_grad_r_buffer, u_fft_grad_c_buffer)
+
+   program.ncsReduceNoise(queue, (1,), (1,),
+                          u_fft_grad_r_buffer,
+                          u_fft_grad_c_buffer,
+                          data_buffer,
+                          gamma_buffer,
+                          otf_mask_buffer,
+                          u_buffer,
+                          iters_buffer,
+                          status_buffer,
+                          numpy.float32(alpha))
+
+   cl.enqueue_copy(queue, u, u_buffer).wait()
+   cl.enqueue_copy(queue, iters, iters_buffer).wait()
+   cl.enqueue_copy(queue, status, status_buffer).wait()
+   queue.finish()
+
+   # Python reference version.
+   ref_u = numpy.zeros(data.size)
+   ref_iters = numpy.zeros_like(iters)
+   ref_status = numpy.zeros_like(status)
+   
+   [py_u_fft_grad_r, py_u_fft_grad_c] = pyRef.createUFFTGrad()
+   pyRef.ncsReduceNoise(py_u_fft_grad_r,
+                        py_u_fft_grad_c,
+                        data,
+                        gamma,
+                        otf_mask_shift,
+                        ref_u,
+                        ref_iters,
+                        ref_status,
+                        numpy.float32(alpha))
+
+   ref_u = numpy.reshape(ref_u, data.shape)
+   norm_diff = numpy.max(numpy.abs(u[:,:] - ref_u[:,:]))/numpy.max(ref_u[:,:])
+   assert(norm_diff < 1.0e-2), str(norm_diff)
+
+def test_ncs_noise_reduction_2():
 
    # Setup
    numpy.random.seed(1)
@@ -49,8 +127,7 @@ def test_ncs_noise_reduction():
    otf_mask = numpy.random.uniform(size = (n_pts, n_pts)).astype(numpy.float32)
    otf_mask_shift = numpy.fft.fftshift(otf_mask)
 
-   # OpenCL Setup.
-   
+   # OpenCL Setup.   
    u_fft_grad_r = numpy.zeros((n_pts * n_pts, n_pts, n_pts)).astype(numpy.float32)
    u_fft_grad_c = numpy.zeros((n_pts * n_pts, n_pts, n_pts)).astype(numpy.float32)
    
@@ -113,3 +190,7 @@ def test_ncs_noise_reduction():
       norm_diff = numpy.max(numpy.abs(u[i,:,:] - ref_u[i,:,:]))/numpy.max(ref_u[i,:,:])
       assert(norm_diff < 1.0e-2), str(norm_diff)
       
+
+if (__name__ == "__main__"):
+   test_ncs_noise_reduction_1()
+   
