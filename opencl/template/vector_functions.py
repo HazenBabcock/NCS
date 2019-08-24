7e9044d751903183eedffe36dc9705d739e6bfdf
Hazen Babcock <hbabcock@mac.com>
2019-08-24 12:37:47 -0400

    Add more vector functions.

--- opencl/template/vector_functions.py
+++ opencl/template/vector_functions.py
@@ -14,13 +14,15 @@ import jinja2
 # Vector copy.
 veccopy_tpl = jinja2.Template("""
 {%- if not_inline -%}
-{{device}}void veccopy({{local}}{{real_type}} *{{v1}}, {{local}}{{real_type}} *{{v2}}, int lid)
+{{device}}void veccopy({{local}}{{real_type}} *{{a1}}, {{local}}{{real_type}} *{{a2}}, int lid)
 {
+  {% if (item_size != "1") -%}
   int i = lid*{{item_size}};
+  {%- endif -%}
   {%- endif -%}
 
   {% for i in indices %}
-  {{v1}}[{{i}}] = {{v2}}[{{i}}];
+  {{a1}}[{{i}}] = {{a2}}[{{i}}];
   {%- endfor -%}
 
 {% if not_inline %}
@@ -29,9 +31,10 @@ veccopy_tpl = jinja2.Template("""
 """)
 
 def veccopy(a1, a2, args):
-    args["a1"] = a1
-    args["a2"] = a2
-    return veccopy_tpl.render(args)
+    tmp = args.copy()
+    tmp["a1"] = a1
+    tmp["a2"] = a2
+    return veccopy_tpl.render(tmp)
 
 
 # Vector negative copy.
@@ -39,7 +42,9 @@ vecncopy_tpl = jinja2.Template("""
 {%- if not_inline -%}
 {{device}}void vecncopy({{local}}{{real_type}} *{{a1}}, {{local}}{{real_type}} *{{a2}}, int lid)
 {
+  {% if (item_size != "1") -%}
   int i = lid*{{item_size}};
+  {%- endif -%}
   {%- endif -%}
 
   {% for i in indices %}
@@ -52,9 +57,10 @@ vecncopy_tpl = jinja2.Template("""
 """)
 
 def vecncopy(a1, a2, args):
-    args["a1"] = a1
-    args["a2"] = a2
-    return vecncopy_tpl.render(args)
+    tmp = args.copy()
+    tmp["a1"] = a1
+    tmp["a2"] = a2
+    return vecncopy_tpl.render(tmp)
 
 
 # Vector dot product (stored in the first element of a1).
@@ -62,8 +68,10 @@ vecdot_tpl = jinja2.Template("""
 {%- if not_inline -%}
 {{device}}void vecdot({{local}}{{real_type}} *{{a1}}, {{local}}{{real_type}} *{{a2}}, {{local}}{{real_type}} *{{a3}}, int lid)
 {
-  int i = lid*{{scale}};
-  {{real_type}} sum = ({{real_type}})0.0;
+  {% if (item_size != "1") -%}
+  int i = lid*{{item_size}};
+  {% endif %}
+  {{real_type}} sum = 0.0{{real_type[0]}};
   {%- endif -%}
 
   {% for i in indices %}
@@ -88,8 +96,155 @@ vecdot_tpl = jinja2.Template("""
 """)
 
 def vecdot(a1, a2, a3, args):
-    args["a1"] = a1
-    args["a2"] = a2
-    args["a3"] = a3
-    return vecdot_tpl.render(args)
+    tmp = args.copy()
+    tmp["a1"] = a1
+    tmp["a2"] = a2
+    tmp["a3"] = a3
+    return vecdot_tpl.render(tmp)
 
+
+# Compare two vectors for equality.
+vecisEqual_tpl = jinja2.Template("""
+{%- if not_inline -%}
+{{device}}void vecisEqual({{local}}{{real_type}} *{{a1}}, {{local}}{{real_type}} *{{a2}}, {{local}}{{real_type}} *{{a3}}, int lid)
+{
+  {% if (item_size != "1") -%}
+  int i = lid*{{item_size}};
+  {% endif %}
+  int sum = 0;
+  {%- endif -%}
+
+  {% for i in indices %}
+  sum += ({{a2}}[{{i}}] != {{a3}}[{{i}}]);
+  {%- endfor %}
+
+  {{a1}}[lid] = sum;
+
+  {{sync_fn}}
+
+  if(lid == 0){
+     for(i=1; i<{{group_size}}; i++){
+        {{a1}}[0] += {{a1}}[i];
+     }
+  } 
+
+  {{sync_fn}}
+
+{% if not_inline %}
+}
+{% endif %}
+""")
+
+def vecisEqual(a1, a2, a3, args):
+    tmp = args.copy()
+    tmp["a1"] = a1
+    tmp["a2"] = a2
+    tmp["a3"] = a3
+    return vecisEqual_tpl.render(tmp)
+
+
+# Float multiply and add, a1 = a2 * a4 + a3 
+vecfma_tpl = jinja2.Template("""
+{%- if not_inline -%}
+{{device}}void vecfma({{local}}{{real_type}} *{{a1}}, {{local}}{{real_type}} *{{a2}}, {{local}}{{real_type}} *{{a3}}, {{real_type}} {{a4}}, int lid)
+{
+  {% if (item_size != "1") -%}
+  int i = lid*{{item_size}};
+  {%- endif -%}
+  {%- endif -%}
+
+  {% for i in indices %}
+  {{a1}}[{{i}}] = fma({{a4}}, {{a2}}[{{i}}], {{a3}}[{{i}}]);
+  {%- endfor -%}
+
+{% if not_inline %}
+}
+{% endif %}
+""")
+
+def vecfma(a1, a2, a3, a4, args):
+    tmp = args.copy()
+    tmp["a1"] = a1
+    tmp["a2"] = a2
+    tmp["a3"] = a2
+    tmp["a4"] = a2
+    return veccopy_tpl.render(tmp)
+
+
+# Float multiply and add in place,  a1 = a2 * a3 + a1.
+vecfmaInplace_tpl = jinja2.Template("""
+{%- if not_inline -%}
+{{device}}void vecfma({{local}}{{real_type}} *{{a1}}, {{local}}{{real_type}} *{{a2}}, {{real_type}} {{a3}}, int lid)
+{
+  {% if (item_size != "1") -%}
+  int i = lid*{{item_size}};
+  {%- endif -%}
+  {%- endif -%}
+
+  {% for i in indices %}
+  {{a1}}[{{i}}] = fma({{a3}}, {{a2}}[{{i}}], {{a1}}[{{i}}]);
+  {%- endfor -%}
+
+{% if not_inline %}
+}
+{% endif %}
+""")
+
+def vecfmaInplace(a1, a2, a3, args):
+    tmp = args.copy()
+    tmp["a1"] = a1
+    tmp["a2"] = a2
+    tmp["a3"] = a2
+    return vecfmaInplace_tpl.render(tmp)
+
+
+# Scale vector in place.
+vecscaleInplace_tpl = jinja2.Template("""
+{%- if not_inline -%}
+{{device}}void vecscaleInplace({{local}}{{real_type}} *{{a1}}, {{local}}{{real_type}} *{{a2}}, int lid)
+{
+  {% if (item_size != "1") -%}
+  int i = lid*{{item_size}};
+  {%- endif -%}
+  {%- endif -%}
+
+  {% for i in indices %}
+  {{a1}}[{{i}}] = {{a2}}[{{i}}] * {{a1}}[{{i}}];
+  {%- endfor -%}
+
+{% if not_inline %}
+}
+{% endif %}
+""")
+
+def vecscaleInplace(a1, a2, args):
+    tmp = args.copy()
+    tmp["a1"] = a1
+    tmp["a2"] = a2
+    return vecscaleInplace_tpl.render(tmp)
+
+
+# Subtract two vectors.
+vecsub_tpl = jinja2.Template("""
+{%- if not_inline -%}
+{{device}}void vecsub({{local}}{{real_type}} *{{a1}}, {{local}}{{real_type}} *{{a2}}, {{local}}{{real_type}} *{{a3}}, int lid)
+{
+  {% if (item_size != "1") -%}
+  int i = lid*{{item_size}};
+  {%- endif -%}
+  {%- endif -%}
+
+  {% for i in indices %}
+  {{a1}}[{{i}}] = {{a2}}[{{i}}] - {{a1}}[{{i}}];
+  {%- endfor -%}
+
+{% if not_inline %}
+}
+{% endif %}
+""")
+
+def vecsub(a1, a2, args):
+    tmp = args.copy()
+    tmp["a1"] = a1
+    tmp["a2"] = a2
+    return vecsub_tpl.render(tmp)
